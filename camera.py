e8ac1b98536cb6006b0ea176dd15fde7109edbeb
Zandermelon <Zandercbrooks@gmail.com>
2026-05-03 22:14:45 -0700

    create camera class for handing video input

---
+++ camera.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2 as cv
+
+
+class Camera:
+        def __init__(self, video=0):
+                self.video = cv.VideoCapture(video)
+                if not self.video.isOpened():
+                        raise Exception("Couldn't open camera")
+                
+        # Captures video input
+        def get_frame(self):
+                ret, frame = self.video.read()
+                if not ret:
+                        return None
+                return frame
+        
+        def release(self):
+                self.video.release()
