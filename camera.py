15af0e335d490f3efc4dc96fc547911b18eeca06
Zandermelon <Zandercbrooks@gmail.com>
2026-05-05 04:33:04 -0700

    Fix Timestamp correctly returning current time in ms && Fix get_frame to return frame in correct RGB format

--- camera.py
+++ camera.py
@@ -1,4 +1,4 @@
-import numpy as np
+import time
 import cv2 as cv
 
 
@@ -7,16 +7,17 @@ class Camera:
                 self.video = cv.VideoCapture(video)
                 if not self.video.isOpened():
                         raise Exception("Couldn't open camera")
+                self.start_ns = time.monotonic_ns()
                 
         # Captures video input
         def get_frame(self):
                 ret, frame = self.video.read()
                 if not ret:
                         return None
-                return frame
+                return cv.cvtColor(frame, cv.COLOR_BGR2RGB) # Convert frame to standard RGB
         
         def release(self):
                 self.video.release()
 
         def get_timestamp(self):
-                return self.video.get(cv.CAP_PROP_POS_MSEC)
+                return (time.monotonic_ns() - self.start_ns) // 1000000 # convert nanoseconds to ms
