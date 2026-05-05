201febff2294246341b50bb4a000cb09159dc26b
Zandermelon <Zandercbrooks@gmail.com>
2026-05-05 05:09:56 -0700

    optimized for video input

--- camera.py
+++ camera.py
@@ -3,21 +3,23 @@ import cv2 as cv
 
 
 class Camera:
-        def __init__(self, video=0):
-                self.video = cv.VideoCapture(video)
-                if not self.video.isOpened():
-                        raise Exception("Couldn't open camera")
-                self.start_ns = time.monotonic_ns()
-                
-        # Captures video input
-        def get_frame(self):
-                ret, frame = self.video.read()
-                if not ret:
-                        return None
-                return cv.cvtColor(frame, cv.COLOR_BGR2RGB) # Convert frame to standard RGB
-        
-        def release(self):
-                self.video.release()
+    def __init__(self, video=0, width=640, height=480):
+        self.video = cv.VideoCapture(video, cv.CAP_DSHOW)
+        if not self.video.isOpened():
+            raise RuntimeError("Couldn't open camera")
 
-        def get_timestamp(self):
-                return (time.monotonic_ns() - self.start_ns) // 1000000 # convert nanoseconds to ms
+        self.video.set(cv.CAP_PROP_FRAME_WIDTH, width)
+        self.video.set(cv.CAP_PROP_FRAME_HEIGHT, height)
+        self.video.set(cv.CAP_PROP_BUFFERSIZE, 1)
+
+        self.start_ns = time.monotonic_ns()
+
+    def get_frame(self):
+        ret, frame = self.video.read()
+        return frame if ret else None
+
+    def get_timestamp(self):
+        return (time.monotonic_ns() - self.start_ns) // 1_000_000
+
+    def release(self):
+        self.video.release()
