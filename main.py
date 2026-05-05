490f504d902a7fab2bfa95fb3f87aa2e883f7225
Zandermelon <Zandercbrooks@gmail.com>
2026-05-05 05:11:18 -0700

    Changed landmarker path to be in main

--- main.py
+++ main.py
@@ -2,33 +2,30 @@ import cv2 as cv
 from camera import Camera
 from landmarker import Landmarker
 
-cam = Camera(0)
-landmarker = Landmarker(cam)
+MODEL_PATH = r"C:\Users\Zander\Desktop\Code\Gestsense\hand_landmarker.task"
 
 
+def main():
+    cam = Camera(0)
+    landmarker = Landmarker(MODEL_PATH)
+
+    try:
+        while True:
+            frame = cam.get_frame()
+            if frame is None:
+                continue
+
+            landmarker.process_frame(frame, cam.get_timestamp())
+            landmarker.draw_landmarks(frame)
+
+            cv.imshow("Gestsense", frame)
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+    finally:
+        cam.release()
+        landmarker.close()
+        cv.destroyAllWindows()
 
 
-try:
-    while True:
-        frame = cam.get_frame()
-
-        if landmarker.camera.get_frame() is None:
-            continue
-
-        landmarker.process_frame()
-        frame = landmarker.draw_landmarks(landmarker.camera.get_frame())
-
-
-        cv.imshow("Gestsense", frame)
-
-        if cv.waitKey(1) & 0xFF == ord("q"):
-            break
-finally:
-    cam.release()
-    cv.destroyAllWindows()
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
