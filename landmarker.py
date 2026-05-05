1b5fe933f764878b21ca2cf0298d6e8133080083
Zandermelon <Zandercbrooks@gmail.com>
2026-05-05 04:13:56 -0700

    Add process function

--- landmarker.py
+++ landmarker.py
@@ -1,6 +1,6 @@
 import mediapipe as mp
+from camera import Camera
 
-model_path = r"C:\Users\Zander\Desktop\Code\Gestsense\hand_landmarker.task"
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -8,18 +8,49 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-# Create a hand landmarker instance with the live stream mode:
-def print_result(result, output_image, timestamp_ms):
-    if result.hand_landmarks:
-        print(f"[{timestamp_ms} ms] hands detected: {len(result.hand_landmarks)}")
-    else:
-        print(f"[{timestamp_ms} ms] no hands")
+class Landmarker:
+    def __init__(self, camera):
+        self.model_path = r"C:\Users\Zander\Desktop\Code\Gestsense\hand_landmarker.task"
+        
+        self.options = HandLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=self.model_path),
+        num_hands=2,
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=self.print_result)
 
-options = HandLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
-with HandLandmarker.create_from_options(options) as landmarker:
+        self.camera = camera
+        
+            
+    # Create a hand landmarker instance with the live stream mode:
+    def print_result(result, output_image, timestamp_ms):
+        if result.hand_landmarks:
+            print(f"[{timestamp_ms} ms] hands detected: {len(result.hand_landmarks)}")
+        else:
+            print(f"[{timestamp_ms} ms] no hands")
 
-    # The landmarker is initialized. Use it here.
-    # ...
+    def process_frame(self):
+        with HandLandmarker.create_from_options(self.options) as landmarker:
+            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=self.camera.get_frame())
+            landmarker.detect_async(image,self.camera.get_timestamp())
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+    
